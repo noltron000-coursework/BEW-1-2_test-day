93cbcb9ea7aee7e23921fceb9d4ae346edd75f1e
Nolan Kovacik <noltron000@gmail.com>
2018-12-05 14:53:00 -0800

    making tests

--- test/sample.test.js
+++ test/sample.test.js
@@ -3,12 +3,16 @@
 // TODO: Challenge
 const chai = require('chai');
 const sinon = require('sinon');
-const assert = require('assert');
+// const assert = require('assert');
 const should = chai.should();
 
 function greaterThanFive(num) {
 	if (num > 5) return true;
 	return false;
+}
+
+function divideByZero(num) {
+	return num / 0
 }
 
 describe('Example Sinon Stub', () => {
@@ -18,4 +22,17 @@ describe('Example Sinon Stub', () => {
 		greaterThanFive(0).should.not.eql(false);
 		done();
 	});
+	it('is a large number', (done => {
+		const bool = greaterThanFive(1000000);
+		bool.should.eql(true);
+		done();
+	}))
 });
+
+describe('Divide By Zero', () => {
+	it('should break', (done) => {
+		const divideByZero = sinon.stub().returns(1 / 0);
+		divideByZero.should.not.eql(1);
+		done();
+	})
+})
